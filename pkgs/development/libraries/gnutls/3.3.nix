10518a2abdbe96a86822c35e842b82bee123e427
Guillaume Maudoux <layus.on@gmail.com>
2016-02-03 23:11:56 +0100

    gnutls: Honor SSL_CERT_FILE

--- pkgs/development/libraries/gnutls/3.3.nix
+++ pkgs/development/libraries/gnutls/3.3.nix
@@ -3,6 +3,8 @@
 callPackage ./generic.nix (args // rec {
   version = "3.3.18";
 
+  patches = [ ./trust-ssl-cert-file_3.3.patch ];
+
   src = fetchurl {
     url = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.3/gnutls-${version}.tar.xz";
     sha256 = "15ckyblhkap3d4sqw0dc9l8wdrnd2aj1fs9m0w0a3bfihvsfg1vs";
--- pkgs/development/libraries/gnutls/3.4.nix
+++ pkgs/development/libraries/gnutls/3.4.nix
@@ -8,6 +8,8 @@ callPackage ./generic.nix (args // rec {
     sha256 = "1v109px1sy1s731fnawzdsvggdswmr7ha9q5lid4v8pzgznmkdgy";
   };
 
+  patches = [ ./trust-ssl-cert-file_3.4.patch ];
+
   # This fixes some broken parallel dependencies
   postPatch = ''
     sed -i 's,^BUILT_SOURCES =,\0 systemkey-args.h,g' src/Makefile.am
