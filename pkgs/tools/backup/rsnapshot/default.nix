0443312fe86679ef8120de4880bbc176f6761fef
Guillaume Maudoux <guillaume.maudoux@uclouvain.be>
2016-05-11 14:43:45 +0200

    rsnapshot

--- pkgs/tools/backup/rsnapshot/default.nix
+++ pkgs/tools/backup/rsnapshot/default.nix
@@ -1,20 +1,5 @@
-{ fetchurl, stdenv, writeText, perl, openssh, rsync, logger,
-  configFile ? "/etc/rsnapshot.conf" }:
+{ fetchurl, stdenv, writeText, perl, openssh, rsync, logger }:
 
-let patch = writeText "rsnapshot-config.patch" ''
---- rsnapshot-program.pl	2013-10-05 20:31:08.715991442 +0200
-+++ rsnapshot-program.pl	2013-10-05 20:31:42.496193633 +0200
-@@ -383,7 +383,7 @@
- 	}
- 	
- 	# set global variable
--	$config_file = $default_config_file;
-+	$config_file = '${configFile}';
- }
- 
- # accepts no args
-'';
-in
 stdenv.mkDerivation rec {
   name = "rsnapshot-1.4.1";
 
@@ -28,8 +13,9 @@ stdenv.mkDerivation rec {
   patchPhase = ''
     substituteInPlace "Makefile.in" --replace \
       "/usr/bin/pod2man" "${perl}/bin/pod2man"
-    patch -p0 <${patch}
   '';
+
+  configureFlags = [ "--sysconfdir=/etc" ];
 
   meta = with stdenv.lib; {
     description = "A filesystem snapshot utility for making backups of local and remote systems";
