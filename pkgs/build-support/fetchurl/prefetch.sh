b4cd771dc5a6fced9a2adea7e567b64f33138f26
Guillaume Maudoux <guillaume.maudoux@uclouvain.be>
2018-02-01 12:45:47 +0100

    fixup! Prefetch with in-place substitution

--- pkgs/build-support/fetchurl/prefetch.sh
+++ pkgs/build-support/fetchurl/prefetch.sh
@@ -1,9 +1,31 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p jshon diffutils
 
-set -x
 set -e
 set -o pipefail
+
+main () {
+    local drvAttr=$1
+    local newVersion='"'$2'"'
+
+    # Get the old hash _before_ bumping the version.
+    local oldHash=$(nixVal "$drvAttr.src.outputHash")
+
+    if [ "$newVersion" != '""' ]; then
+        log "Bumping version..."
+        local oldVersion=$(nixVal "$drvAttr._args.version")
+        local versionFile=$(nixAttrFile "$drvAttr._args" '"version"')
+        update "$versionFile" "$oldVersion" "$newVersion"
+    fi
+
+    log "Prefetching source..."
+    local newHash='"'$(nix-prefetch-url -A "$drvAttr.src")'"'
+
+    log "Rewriting hash..."
+    local hashMethod=$(nixVal "$drvAttr.src.outputHashAlgo")
+    local hashFile=$(nixAttrFile "$drvAttr.src._args" "$hashMethod")
+    update "$hashFile" "$oldHash" "$newHash"
+}
 
 update () {
     local file=$1 old=$2 new=$3
@@ -12,50 +34,28 @@ update () {
     sed -e "s/$old/$new/" -i "$file"
 }
 
+nixVal () {
+    local expr=$1; shift
 
-nix-overlay () {
-    expr=$1; shift
-    nix-instantiate --eval --strict -E "with (import ./. {
-      overlays = [ (self: super: with super; {
-        fetchurl = _args: (fetchurl _args) // { inherit _args; };
-        stdenv = stdenv // {
-          mkDerivation = _args: (stdenv.mkDerivation _args) // { inherit _args; };
-        };
-      })];
-    }).pkgs; $expr" $@
+    nix-instantiate --eval --strict -E "
+      #begin:nix
+      with (import ./. {
+        overlays = [ (self: super: with super; {
+          fetchurl = _args: (fetchurl _args) // { inherit _args; };
+          stdenv = stdenv // {
+            mkDerivation = _args: (stdenv.mkDerivation _args) // { inherit _args; };
+          };
+        })];
+      }).pkgs;
+      #end:nix
+    $expr" $@
 }
 
-getAttr () {
-    nix-overlay "$@"
+nixAttrFile () {
+    nixVal "builtins.unsafeGetAttrPos $2 $1" --json | jshon -e file -u
 }
 
-getAttrFile () {
-    nix-overlay "builtins.unsafeGetAttrPos $2 $1" --json | jshon -e file -u
-}
+log () { echo >&2; echo "$@" >&2; }
 
-log () {
-    echo >&2
-    echo "$@" >&2
-}
-
-
-attr=$1
-newVersion='"'$2'"'
-
-oldHash=$(getAttr "$1.src.outputHash")
-
-if [ -n "$2" ]; then
-    log "Bumping version..."
-    oldVersion=$(getAttr "$1._args.version")
-    versionFile=$(getAttrFile "$1._args" '"version"')
-    update "$versionFile" "$oldVersion" "$newVersion"
-fi
-
-log "Prefetching source..."
-newHash='"'$(nix-prefetch-url -A "$1.src")'"'
-
-log "Rewriting hash..."
-hashMethod=$(getAttr "$1.src.outputHashAlgo")
-hashFile=$(getAttrFile "$1.src._args" "$hashMethod")
-update "$hashFile" "$oldHash" "$newHash"
-
+# Run the stuff!
+main "$@"
