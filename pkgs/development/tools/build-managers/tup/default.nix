19e381f9204cdecdd7801f583c7bc663b6c160e8
Guillaume Maudoux <guillaume.maudoux@uclouvain.be>
2018-08-06 14:49:58 +0200

    tup: 0.7.5 -> 0.7.7

--- pkgs/development/tools/build-managers/tup/default.nix
+++ pkgs/development/tools/build-managers/tup/default.nix
@@ -2,29 +2,27 @@
 
 stdenv.mkDerivation rec {
   name = "tup-${version}";
-  version = "0.7.5";
+  version = "0.7.7";
 
   src = fetchFromGitHub {
     owner = "gittup";
     repo = "tup";
     rev = "v${version}";
-    sha256 = "0jzp1llq6635ldb7j9qb29j2k0x5mblimdqg3179dvva1hv0ia23";
+    sha256 = "004i54vwznmdxzh6cpqq1sdns5lbrgab57axlvi9s09a31kks7q1";
   };
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ fuse ];
 
   configurePhase = ''
-    sed -i 's/`git describe`/v${version}/g' Tupfile
+    sed -i 's/`git describe`/v${version}/g' src/tup/link.sh
   '';
 
   # Regular tup builds require fusermount to have suid, which nix cannot
   # currently provide in a build environment, so we bootstrap and use 'tup
   # generate' instead
   buildPhase = ''
-    ./build.sh
-    ./build/tup generate script.sh
-    ./script.sh
+    ./bootstrap-nofuse.sh
   '';
 
   installPhase = ''
