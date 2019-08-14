fecd6fb9f7ac010eaeae4cd54eba32f5e1abf4b5
Guillaume Maudoux <guillaume.maudoux@uclouvain.be>
2019-08-14 14:41:52 +0200

    Wrap everything in a FHSUserEnv for binaries need it

--- pkgs/misc/drivers/canon-cups-capt/default.nix
+++ pkgs/misc/drivers/canon-cups-capt/default.nix
@@ -1,62 +1,35 @@
-{ stdenv, callPackage,
-  tree
+{ stdenv
+, runCommand
+, callPackage
+, tree
+, buildFHSUserEnv
+, makeWrapper
+, writeShellScriptBin
 }:
 
 let
   common = callPackage ./common.nix {};
   capt = callPackage ./capt.nix { cndrvcups-common = common;};
   doc = callPackage ./doc.nix {};
-in
-  stdenv.mkDerivation rec {
+  fhs-runner = writeShellScriptBin "fhs-runner" ''
+    prog=$1
+    shift
+    exec /bin/$prog "$@"
+  '';
+  fhsEnv = buildFHSUserEnv {
+    name = "canon-capt-driver-env";
+    targetPkgs = pkgs: [ common capt fhs-runner ];
+    runScript = "/bin/fhs-runner";
+  };
+  fhsEnvExecutable = "${fhsEnv}/bin/${fhsEnv.name}";
+
+in runCommand "canon-capt-driverpack" {
     pname = "canon-capt-driverpack";
     version = "2.71";
 
     src = null;
 
-    patches = [ ./0002-add-ccpd-service.patch ];
-
-    buildInputs = [
-      common capt doc
-      tree
-    ];
-
-    # install directions based on arch PKGBUILD file
-    # https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=capt-src
-
-    phases = [ "patchPhase" "installPhase" ];
-
-    installPhase = ''
-      set -ex
-
-      mkdir -p $out
-
-      ##NOTE: list output of `common` and `capt` for debugging
-      tree ${common} > $out/common_build.log
-      tree ${capt} > $out/capt_build.log
-      tree ${doc} > $out/doc_build.log
-
-      ##TODO: check how Nix's systemd works
-      ## Installation of the custom Arch Linux CCPD systemd service
-      substituteInPlace ccpd.service \
-        --replace ExecStart=/usr/bin/ccpd ExecStart=${capt}/bin/ccpd
-
-      install -dm755 $out/lib/systemd/system/
-      install -Dm664 ccpd.service $out/lib/systemd/system/ccpd.service
-
-      ###################################################################
-      ##FIXME: the problematic code collected here to be able to cache `capt.nix` build
-      ##       move the corresponding line back to where is should belong
-
-      ##FIXME: stuck with invalid group `lp`
-      # install -dm750 -o root -g lp $out/var/captmon/
-
-      ##FIXME: how to install a ppd to cups?, need to?
-      ##       capt's ppd files are in ${capt}/share/cups/model
-      # install -dm755 $out/share/ppd/cupsfilters/
-      # for _f in ${capt}/share/cups/model/CN*CAPT*.ppd; do
-      #   ln -s $_f $out/share/ppd/cupsfilters
-      # done
-    '';
+    buildInputs = [ makeWrapper ];
 
     meta = with stdenv.lib; {
       description = "Canon CAPT driver pack";
@@ -74,4 +47,37 @@ in
       # license = licenses.gpl3Plus;
     };
 
-  }
+  } ''
+    mkdir -p $out/bin
+    function _redirect() {
+      local target=$1 name=$(basename "$1")
+      cat >$out/bin/$name <<EOF
+    #! ${stdenv.shell} -e
+    exec ${fhsEnvExecutable} "$name" "$@"
+    EOF
+      chmod +x $out/bin/$name
+    }
+    for target in ${common}/bin/* ${capt}/bin/* ; do
+      _redirect "$target"
+    done
+
+    mkdir -p $out/share
+    ln -sn ${capt}/share/cups $out/share/cups
+
+    mkdir -p $out/lib/system/systemd
+    cat > $out/lib/system/systemd/ccpd.service <<EOF
+    # original : https://aur.archlinux.org/cgit/aur.git/plain/ccpd.service?h=capt-src
+
+    [Unit]
+    Description=Canon CAPT daemon
+    Requires=org.cups.cupsd.service
+    After=org.cups.cupsd.service
+
+    [Service]
+    Type=forking
+    ExecStart=/usr/bin/ccpd
+
+    [Install]
+    WantedBy=printer.target
+    EOF
+  ''
